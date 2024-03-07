1776cf61cb9841d21898825110d440b3a9675612
RizkyPratama05 <rizkypp428@gmail.com>
2024-03-07 20:28:36 +0700

    menambahkan program utama

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -60,6 +60,14 @@ void display() {
 	cout << "jumlah pass = " << n - 1 << endl;
 	cout << endl;
 }
+int main() {
+	input(); // call the input function to get user input
+	bubbleshortarray(); //call the selection sort function
+	display(); //display the sorted array
+	system("pause");
+
+	return 0;
+}
 
 
 
